ddb2548d9f9566ca7989bdf9fb321b12be682ba0
vincent <vincent102416849@gmail.com>
2020-07-09 19:30:55 +0200

    Initial commit

---
+++ Assets/Scripts/GMTKCounter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GMTKCounter : MonoBehaviour
+{
+    public TextMeshProUGUI countDownTMP;
+    public string endTimeStr;
+    
+    void Update()
+    {
+        var currentTime = DateTime.Now;
+        var endTime = DateTime.Parse(endTimeStr);
+        var timeSpan = endTime - currentTime;
+        countDownTMP.SetText($"{timeSpan.Days} days {timeSpan.Hours} hours {timeSpan.Minutes} minutes {timeSpan.Seconds} seconds");
+    }
+}
