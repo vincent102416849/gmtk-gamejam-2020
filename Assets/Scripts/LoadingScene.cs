8cb9e22aaca9b7c1522e69028d818c006b98aaff
vincent <vincent102416849@gmail.com>
2020-07-11 15:30:38 +0200

    FMOD loading test

---
+++ Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        if (FMODUnity.RuntimeManager.HasBankLoaded("Master Bank"))
+        {
+            Debug.Log("Master Bank Loaded");
+            SceneManager.LoadScene("Room_1", LoadSceneMode.Single);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
