c29fa38a5c9612f090ae1547b6d213c49ff48ff1
vincent <vincent102416849@gmail.com>
2020-07-11 13:24:57 +0200

    Project meta

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
