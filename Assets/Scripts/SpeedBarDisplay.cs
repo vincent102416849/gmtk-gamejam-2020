2a807ae52a5562a3bc1c4212154f752b9188ccd3
vincent <vincent102416849@gmail.com>
2020-07-11 19:42:46 +0200

    Preview

---
+++ Assets/Scripts/SpeedBarDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedBarDisplay : MonoBehaviour
+{
+    public Player player;
+    public Image speedAmountImage;
+    public List<Sprite> speedAmountSpriteList;
+
+    void Update()
+    {
+        var value = Mathf.CeilToInt(player.moveSpeed);
+        print(value);
+        speedAmountImage.sprite = speedAmountSpriteList[value];
+    }
+}
