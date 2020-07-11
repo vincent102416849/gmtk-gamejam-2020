0f94ed93ee4742134504d8b88dfd5bf411070e2a
vincent <vincent102416849@gmail.com>
2020-07-11 17:43:56 +0200

    Landing page

---
+++ Assets/Scripts/LandingScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LandingScene : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Room_1");
+    }
+
+    public void Option()
+    {
+
+    }
+
+    public void Credit()
+    {
+
+    }
+}
