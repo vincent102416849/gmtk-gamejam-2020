b1cec24bd161939b0a82f964a2fe75e46c7dc948
Michael Clarke <michaelclarkeson@gmail.com>
2020-07-11 19:52:17 +0100

    FMOD

---
+++ Assets/FmodManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FmodManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (FMODUnity.RuntimeManager.HasBankLoaded("Master"))
+        {
+            Debug.Log("Master");
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Music");
+          
+        }
+
+
+
+    }
+}
