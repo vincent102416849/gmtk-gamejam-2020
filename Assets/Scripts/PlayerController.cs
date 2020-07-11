3e66b1171bbc7194dcb7b87b8694dfd507f2fbd8
vincent <vincent102416849@gmail.com>
2020-07-11 10:40:38 +0200

    First roomgit add .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Param")]
     public float moveSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [Header("Config")]
+    public new Rigidbody rigidbody;
+
     void Update()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
-        var delta = new Vector3(horizontal, vertical, 0f) * moveSpeed * Time.deltaTime;
-        transform.Translate(delta);
+        var delta = new Vector3(horizontal, vertical, 0f) * moveSpeed;
+        rigidbody.velocity = delta;
     }
 }
