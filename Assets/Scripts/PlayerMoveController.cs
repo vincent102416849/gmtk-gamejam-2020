f0237d5d46b7eee712e840f5ac15a73bcb16bafa
vincent <vincent102416849@gmail.com>
2020-07-11 10:58:50 +0200

    Player orientation

---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    [Header("Config")]
+    public Player player;
+    public new Rigidbody rigidbody;
+
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        var velocity = new Vector3(horizontal, vertical, 0f) * player.moveSpeed;
+        rigidbody.velocity = velocity;
+        if (Mathf.Abs(horizontal) > 0.01f)
+            transform.rotation = Quaternion.Euler(0f, 0f, horizontal > 0f ? 0f : 180f);
+        if (Mathf.Abs(vertical) > 0.01f)
+            transform.rotation = Quaternion.Euler(0f, 0f, vertical > 0f ? 90f : 270f);
+    }
+}
