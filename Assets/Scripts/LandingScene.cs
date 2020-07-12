ba1f8324e16e6d6b84ff8e447acdd22fe82c6223
MatthewJWoolley <matthewjwoolley@gmail.com>
2020-07-12 13:34:12 +0100

    Landing scene update WIP

--- Assets/Scripts/LandingScene.cs
+++ Assets/Scripts/LandingScene.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LandingScene : MonoBehaviour
 {
+   public GameObject Text1;
+   public GameObject Text2;
+   public GameObject Text3;
+   public GameObject Text4;
+   public GameObject TextAndTutorial;
+
     public void Play()
     {
-        SceneManager.LoadScene("Room_1");
+      Debug.Log("Play button pressed");
+      StartCoroutine(FadeOutRoutine(Text1));
     }
 
     public void Option()
@@ -19,4 +27,11 @@ public class LandingScene : MonoBehaviour
     {
 
     }
+
+    private IEnumerator FadeOutRoutine(GameObject card) {
+      card.SetActive(true);
+      Image image = card.GetComponent<Image>();
+      image.color = new Color(1f,1f,1f,1f);
+      yield return null;
+   }
 }
